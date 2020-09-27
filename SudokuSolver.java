9aca3541f9e9e827a15c0876c83d7156c631e224
Jelle Z <public@jellezwie.rs>
2020-09-27 13:57:51 +0200

    Add additional comments

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -34,17 +34,17 @@ class SudokuSolver {
     // Is there a conflict when we fill in d at position (r, c)?
     boolean givesConflict(int r, int c, int d) {
 
-        return false;
+        return false; // If not, return false to indicate there's no conflict
     }
 
     // Is there a conflict when we fill in d in row r?
     boolean rowConflict(int r, int d) {
         for (int i = 0; i < SUDOKU_SIZE - 1; i++) {
             if (d == grid[r][i]) {
-                return true; // Check for conflicts in row
+                return true; // If d matches any number in the row, return true
             }
         }
-        return false;
+        return false; // If not, return false to indicate there's no conflict
     }
 
     // Is there a conflict in column c when we fill in d?
@@ -52,42 +52,42 @@ class SudokuSolver {
 
         for (int i = 0; i < SUDOKU_SIZE - 1; i++) {
             if (d == grid[i][c]) {
-                return true; // Check for conficts in column
+                return true; // If d matches any number in the column, return true
             }
         }
-        return false;
+        return false; // If not, return false to indicate there's no conflict
     }
 
     // Is there a conflict in the box at (r, c) when we fill in d?
     boolean boxConflict(int r, int c, int d) {
 
-        double indexBoxRowCalculation = (r / SUDOKU_BOX_DIMENSION);
-        int indexBoxRow = (int)indexBoxRowCalculation;
+        double indexBoxRowCalculation = (r / SUDOKU_BOX_DIMENSION); // Gives a decimal number. The number before the decimal point is the index of the row of the box.
+        int indexBoxRow = (int)indexBoxRowCalculation; // Convert decimal number to integer
         
-        double indexBoxColumnCalculation = (c / SUDOKU_BOX_DIMENSION);
-        int indexBoxColumn = (int)indexBoxColumnCalculation;
+        double indexBoxColumnCalculation = (c / SUDOKU_BOX_DIMENSION); // Gives a decimal number. The number before the decimal point is the index of the column of the box.
+        int indexBoxColumn = (int)indexBoxColumnCalculation; // Convert decimal number to integer
         
         for (int i = 0; i < 3; i++) { // For each row
             for (int k = 0; k < 3; k++) { // For each column
-                int startingRowIndex = indexBoxRow * 3;
-                int startingColumnIndex = indexBoxColumn * 3;
-                if (grid[startingRowIndex + i][startingColumnIndex + k] == d) {
-                    return true;
+                int startingGridRowIndex = indexBoxRow * 3; // The most left index of the box on the grid
+                int startingGridColumnIndex = indexBoxColumn * 3; // The most top index of the box on the grid
+                if (grid[startingGridRowIndex + i][startingGridColumnIndex + k] == d) {
+                    return true; // If d matches any number in the box, return true
                 }
             }
         }
-        return false;
+        return false; // If not, return false to indicate there's no conflict
     }
 	
 	// Is there a conflict in the asterisk when we fill in d?
 	boolean asteriskConflict(int r, int c, int d) {
         if ((r == 2 && c == 2) || (r == 1 && c == 4) || (r == 2 && c == 6) || (r == 4 && c == 1) || (r == 4 && c == 4) || (r == 4 && c == 7) || (r == 6 && c == 2) || (r == 7 && c == 4) || (r == 6 && c == 6)) {
             if ((d == grid[2][2]) || (d == grid[1][4]) || (d == grid[2][6]) || (d == grid[4][1]) || (d == grid[4][4]) || (d == grid[4][7]) || (d == grid[6][2] || (d == grid[7][4]) || (d == grid[6][6]))) {
-                return true;
+                return true; // If d matches any number in the in the asterisk, return true
             }
         }
 
-        return false;
+        return false; // If not, return false to indicate there's no conflict
     
     }
 
@@ -98,6 +98,7 @@ class SudokuSolver {
             for (int k = 0; k < SUDOKU_SIZE - 1; k++) { // For each column
                 if (grid[i][k] == 0) {
                     // This is the next empty square in reading order
+                    // TODO  what should it do in that case?
                 }
             }
         }
@@ -116,11 +117,13 @@ class SudokuSolver {
             System.out.print("-");
         }
         System.out.print("+");
+
+        // TODO we still need to print the actual numbers
     }
 
     // Run the actual solver.
     void solveIt() {
-        // TODO
+        // TODO program the solver
     }
 
     public static void main(String[] args) {
